fee25e966fc28fa788dd4a2f2306bce09ab911bd
Sabelo <sabelo.sethu@yahoo.com>
2016-02-24 09:24:56 -0800

    updated syntax for config files

--- server/config/setup.py
+++ server/config/setup.py
@@ -1,4 +1,5 @@
 import subprocess
+import textwrap
 class Nobu:
     def __init__(self, ssid, password):
         self.ssid = ssid
@@ -8,11 +9,11 @@ class Nobu:
         interfaces_conf = self.create_interfaces_conf()
         dhcpd_conf = self.create_dhcpd_conf()
         # create the necessary linux commands to create an AccessPoint
-        hostapd_cmd = 'sudo hostapd /etc/hostapd/hostapd.conf'
-        interace_cmd = ['sudo ifdown wlan0','sudo ifconfig wlan0 192.168.42.1', 'sudo ifup wlan0']
-        dhcpd_cmd = 'sudo dhcpd -cf /etc/dhcp/dhcpd.conf'
+        hostapd_cmd = 'sudo hostapd hostapd.conf.ap &' # run as daemon
+        interface_cmd = ['sudo ifdown wlan0','sudo ifconfig wlan0 192.168.42.1', 'sudo ifup wlan0']
+        dhcpd_cmd = 'sudo dhcpd -cf dhcpd.conf.ap'
         conf_files = [('hostapd.conf.ap', hostapd_conf, hostapd_cmd), \
-            ('interfaces.ap', interfaces_conf, interace_cmd ), \
+            ('interfaces.ap', interfaces_conf, interface_cmd ), \
             ('dhcpd.conf.ap', dhcpd_conf, dhcpd_cmd)]
         # write conf files to disk. Location is current directory '.'
         for conf in conf_files:
@@ -25,72 +26,75 @@ class Nobu:
         f.write(text)
     # Returns the hostapd configuration text
     def create_hostapd_conf(self):
-        hostapd_conf = """# Basic configuration
-interface=wlan0
-ssid=%s
-channel=1
+        hostapd_conf = textwrap.dedent("""\
+            # Basic configuration
+            interface=wlan0
+            ssid=%s
+            channel=1
 
-# WPA and WPA2 configuration
-macaddr_acl=0
-auth_algs=1
-ignore_broadcast_ssid=0
-wpa=3
-wpa_passphrase=%s
-wpa_key_mgmt=WPA-PSK
-wpa_pairwise=TKIP
-rsn_pairwise=CCMP
+            # WPA and WPA2 configuration
+            macaddr_acl=0
+            auth_algs=1
+            ignore_broadcast_ssid=0
+            wpa=3
+            wpa_passphrase=%s
+            wpa_key_mgmt=WPA-PSK
+            wpa_pairwise=TKIP
+            rsn_pairwise=CCMP
 
-# Hardware configuration
-driver=rtl871xdrv
-ieee80211n=1
-hw_mode=g
-device_name=RTL8192CU
-manufacturer=Realtek""" % (self.ssid, self.password)
-    return hostapd_conf
+            # Hardware configuration
+            driver=rtl871xdrv
+            ieee80211n=1
+            hw_mode=g
+            device_name=RTL8192CU
+            manufacturer=Realtek""") % (self.ssid, self.password)
+        return hostapd_conf
     # returns the dhcpd configuration text
     def create_dhcpd_conf(self):
-        dhcpd_conf = """# The ddns-updates-style parameter controls whether or not the server will
-# attempt to do a DNS update when a lease is confirmed. We default to the
-# behavior of the version 2 packages ('none', since DHCP v2 didn't
-# have support for DDNS.)
-ddns-update-style none;
+        dhcpd_conf = textwrap.dedent("""\
+            # The ddns-updates-style parameter controls whether or not the server will
+            # attempt to do a DNS update when a lease is confirmed. We default to the
+            # behavior of the version 2 packages ('none', since DHCP v2 didn't
+            # have support for DDNS.)
+            ddns-update-style none;
 
-default-lease-time 600;
-max-lease-time 7200;
+            default-lease-time 600;
+            max-lease-time 7200;
 
-# If this DHCP server is the official DHCP server for the local
-# network, the authoritative directive should be uncommented.
-authoritative;
+            # If this DHCP server is the official DHCP server for the local
+            # network, the authoritative directive should be uncommented.
+            authoritative;
 
-# Use this to send dhcp log messages to a different log file (you also
-# have to hack syslog.conf to complete the redirection).
-log-facility local7;
+            # Use this to send dhcp log messages to a different log file (you also
+            # have to hack syslog.conf to complete the redirection).
+            log-facility local7;
 
-subnet 192.168.42.0 netmask 255.255.255.0 {
-  range 192.168.42.10 192.168.42.50;
-  option broadcast-address 192.168.42.255;
-  option routers 192.168.42.1;
-  default-lease-time 600;
-  max-lease-time 7200;
-  option domain-name "local";
-  option domain-name-servers 8.8.8.8, 8.8.4.4;
-}
-"""
-    return dhcpd_conf
+            subnet 192.168.42.0 netmask 255.255.255.0 {
+              range 192.168.42.10 192.168.42.50;
+              option broadcast-address 192.168.42.255;
+              option routers 192.168.42.1;
+              default-lease-time 600;
+              max-lease-time 7200;
+              option domain-name "local";
+              option domain-name-servers 8.8.8.8, 8.8.4.4;
+            }
+            """)
+        return dhcpd_conf
     # returns the interfaces configuration text
     def create_interfaces_conf(self):
-        interfaces_conf = """auto lo
-iface lo inet loopback
-iface eth0 inet dhcp
-allow-hotplug wlan0
-iface wlan0 inet static
-  address 192.168.42.1
-  netmask 255.255.255.0
-"""
-    return interfaces_conf
-    
+        interfaces_conf = textwrap.dedent("""\
+            auto lo
+            iface lo inet loopback
+            iface eth0 inet dhcp
+            allow-hotplug wlan0
+            iface wlan0 inet static
+              address 192.168.42.1
+              netmask 255.255.255.0
+            """)
+        return interfaces_conf
     # helper function to write linux commands
     def run_command(self, args):
+        print(args)
         cmd = subprocess.Popen(args, shell=isinstance(args, basestring), stdout=subprocess.PIPE)
         (stdoutdata, stderrdata) = cmd.communicate()
         return stdoutdata
